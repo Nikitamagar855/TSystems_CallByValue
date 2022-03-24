4e42e010d439c354813ddd648444c08e9f861f59
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:29:44 +0530

    Add project files.

---
+++ TSystems_CallByValue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_CallByValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============CallByValue==============");
+            int val = 50;
+            Console.WriteLine($"the value passed before passing to the function::{value}");
+            Program obj = new Program();
+            obj.Show(val);
+            Console.WriteLine($"");
+
+        }
+    }
+
+}
